cc4b56bd1c12385da7fc5028ced487bf743a277c
Pedro <pedrokaz.henrique@gmail.com>
2022-01-17 20:41:23 -0300

    Home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,80 @@
+import { Button, Box } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { useInfiniteQuery } from 'react-query';
+
+import { Header } from '../components/Header';
+import { CardList } from '../components/CardList';
+import { api } from '../services/api';
+import { Loading } from '../components/Loading';
+import { Error } from '../components/Error';
+
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface ImageResponse {
+  data: Card[];
+  after: string | null;
+}
+
+export default function Home(): JSX.Element {
+  async function getImages({ pageParam = null }): Promise<ImageResponse> {
+    const { data } = await api('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return data;
+  }
+
+  const {
+    data,
+    isLoading,
+    isError,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery('images', getImages, {
+    getNextPageParam: lastPage => lastPage.after ?? null,
+    // getNextPageParam: lastPage => (lastPage?.after ? lastPage.after : null),
+  });
+
+  const formattedData = useMemo(() => {
+    return data?.pages.flatMap(item => {
+      return item.data.flat();
+    });
+  }, [data]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <Error />;
+  }
+
+  return (
+    <>
+      <Header />
+
+      <Box maxW={1120} px={20} mx="auto" my={20}>
+        <CardList cards={formattedData} />
+
+        {hasNextPage && (
+          <Button
+            mt="2rem"
+            disabled={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
+      </Box>
+    </>
+  );
+}
